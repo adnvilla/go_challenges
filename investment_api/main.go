314f4bc1e1e6633c02cb53424440bc8b9e82718e
adnvilla <adnvilla@gmail.com>
2024-04-12 23:26:34 -0500

    add test cases

--- investment_api/main.go
+++ investment_api/main.go
@@ -6,22 +6,26 @@ import (
 )
 
 // Función para encontrar todas las combinaciones posibles de sumas de los números primos dados, permitiendo duplicación
-func findCombinations(primes []int, target int) int {
+func findCombinations(primes []int, target int, initComb []int, findOnce bool) (int, [][]int) {
 	count := 0
+	combis := [][]int{}
 	//[]int{3, 5, 7} Se establece para que al menos este presente cada numero primo en la combinacion que se encuentre
 	// Si desea que se permita combinaciones que satisfagan la suma con un solo numero primo, establecer []int{}
-	combinations(primes, target, []int{3, 5, 7}, &count)
-	return count
+	combinations(primes, target, initComb, findOnce, &count, &combis)
+	return count, combis
 }
 
 // Función recursiva para generar todas las combinaciones posibles con duplicación
-func combinations(primes []int, target int, currentComb []int, count *int) {
+func combinations(primes []int, target int, currentComb []int, findOnce bool, count *int, combis *[][]int) {
 	sum := 0
 	for _, num := range currentComb {
 		sum += num
 	}
 	if sum == target {
 		fmt.Println(currentComb) // printSortedComb para revisar las combinaciones
+		tmp := make([]int, len(currentComb))
+		copy(tmp, currentComb)
+		*combis = append(*combis, tmp)
 		*count++
 		return
 	}
@@ -29,8 +33,8 @@ func combinations(primes []int, target int, currentComb []int, count *int) {
 		return
 	}
 	for i := 0; i < len(primes); i++ {
-		combinations(primes, target, append(currentComb, primes[i]), count)
-		if *count > 0 {
+		combinations(primes, target, append(currentComb, primes[i]), findOnce, count, combis)
+		if findOnce && *count > 0 {
 			return // Si ya se encontró una combinación válida, terminar la verificación de combinaciones
 		}
 	}
@@ -50,9 +54,10 @@ func printSortedComb(comb []int) {
 func main() {
 	// Ya que se garantiza que son multiplos de 100, entonces podemos inferir que los valores 300, 500, y 700 son los numeros primos de 3, 5 y 7
 	primes := []int{3, 5, 7}
-	targetNumber := 30
-	countCombinations := findCombinations(primes, targetNumber)
+	targetNumber := 4
+	countCombinations, combis := findCombinations(primes, targetNumber, []int{3, 5, 7}, false)
 	fmt.Println("Número total de combinaciones:", countCombinations)
+	fmt.Println(combis)
 
 	// Para obtener todas las combinaciones, quitar la restriccion de cuando encuentre una combinacon que satisfaga las condiciones regrese esta combinacion
 }
---
+++ investment_api/main_test.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestInvestment(t *testing.T) {
+
+	investment := []struct {
+		tarjet        int
+		findExpected  bool
+		expectedCombi []int
+	}{
+		{30, true, []int{3, 3, 5, 5, 7, 7}},
+		{67, true, []int{3, 3, 3, 3, 3, 3, 3, 5, 5, 5, 5, 5, 7, 7, 7}},
+		{4, false, []int{}},
+	}
+
+	primes := []int{3, 5, 7}
+	initComb := []int{3, 5, 7}
+
+	for _, inv := range investment {
+		_, combis := findCombinations(primes, inv.tarjet, initComb, false)
+
+		find := false
+		for i := 0; i < len(combis); i++ {
+			currentComb := combis[i]
+			slices.Sort(currentComb)
+
+			if testEq(inv.expectedCombi, currentComb) {
+				find = true
+				break
+			}
+		}
+
+		if !find && inv.findExpected {
+			t.Errorf("The case tarjet %v dont find the expected combi %v", inv.tarjet, inv.expectedCombi)
+		}
+
+		if find && !inv.findExpected {
+			t.Errorf("The case tarjet %v find the expected combi %v", inv.tarjet, inv.expectedCombi)
+		}
+	}
+}
+
+func testEq(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
